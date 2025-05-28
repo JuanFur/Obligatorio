716e66596d2dbaed27d3bbada2db8d5fd1c6d80c
Gaspar512 <gasparreimundez@icloud.com>
2025-05-28 08:48:29 -0300

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -6,49 +6,33 @@ def generar_id():
     contador_id += 1
     return id_actual
 
-
+    
 class Cliente():
     
-    def __init__(self,telefono,correo_electronico):
-        self.telefono = telefono
-        self.correo = correo_electronico
-        self.id = generar_id()
-    
-    def info_cliente (self):
-       print(f"ID: {self.id}")
-       print(f"Correo: {self.correo}")
-       print(f"tel: {self.telefono}")
-    
+    def __init__(self,tipo):
+       self.id = generar_id()
+       self.tipo =  tipo
+        
         
 class ClienteParticular (Cliente):
    def __init__(self,nombre, cedula, telefono, correo_electronico):
-      super().__init__(telefono, correo_electronico)
+      super().__init__("Particular")
       self.nombre = nombre
       self.cedula = cedula
-   def info_cliente(self):
-     print("Tipo: Particular")
-     print(f"Cedula: {self.cedula}")
-     print(f"Nombre: {self.nombre}")
-     super().info_cliente()
-
+      self.telefono = telefono
+      self.correo = correo_electronico
+  
 
 
 class Empresa(Cliente):
    def __init__(self, rut, nombre,pag_web, telefono, correo_electronico):
-      super().__init__(telefono, correo_electronico)
+      super().__init__("Empresa")
       self.rut = rut
       self.nombre = nombre
       self.pagina = pag_web
+      self.telefono = telefono
+      self.correo = correo_electronico
 
-   def info_cliente(self):
-       print("Tipo: Empresa")
-       print(f"RUT: {self.rut}")
-       print(f"Nombre: {self.nombre}")
-       print(f"Pagina web: {self.pagina}")
-       super().info_cliente()
-
-       
-       
 
 def registro_cliente():
    tipo = input("Seleccionar tipo cliente:\n1 Particular\n2 Empresa\n>")
@@ -58,7 +42,7 @@ def registro_cliente():
         telefono = input("Ingrese teléfono: ")
         correo = input("Ingrese correo electrónico: ")
         cliente = ClienteParticular(cedula, nombre, telefono, correo)
-   if tipo == "2":
+   elif tipo == "2":
         rut = input("Ingrese RUT: ")
         nombre = input("Ingrese nombre de la empresa: ")
         pagina = input("Ingrese página web: ")
@@ -67,9 +51,10 @@ def registro_cliente():
         cliente = Empresa(rut, nombre, pagina, telefono, correo)
    else:
        print ("Tipo cliente no disponible")
-       return
-   print("\n Cliente registrado")
-   cliente.info_cliente()
-
-
+       return None
+   
+   print("\nCliente registrado:")
+   for clave, valor in vars(cliente).items():
+        print(f"{clave}: {valor}")
+   
 registro_cliente()
