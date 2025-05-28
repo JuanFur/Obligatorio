f5cc85a50941c0a1c6cbbfcbf0a304061f96c61a
Nacho2917 <ignacioreyesfalero@gmail.com>
2025-05-28 08:32:47 -0300

    Update Reposicion.py

--- Reposicion.py
+++ Reposicion.py
@@ -1,8 +1,23 @@
 from datetime import datetime
 
-class Reposición:
+class Reposicion:
     def __init__(self, pieza, cantidad_lotes):
         self.pieza = pieza
         self.cantidad_lotes = cantidad_lotes
         self.fecha = datetime.now()
+
+        self.cantidad_total = self.tamanio_lote * self.cantidad_lotes
+        self.costo = self.pieza.costo * self.cantidad_total
+
+        self.actualizarstock()
+
+        def actualizarstock(self):
+            self.pieza.cantidad_disponible += self.cantidad_total
+
+        def __str__(self):
+            return (f"Reposición de {self.pieza.descripcion}, {self.cantidad_lotes} lotes"
+                    f"en {self.fecha}"
+                    f"Stock sumado: {self.cantidad_total}"
+                    f"Costo total: USD {self.costo_total}")
+
         
