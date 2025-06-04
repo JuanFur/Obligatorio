d88e0b681593eab2d4dcdb16d4ef93e38b65cc61
Nacho2917 <ignacioreyesfalero@gmail.com>
2025-06-04 07:42:19 -0300

    1

--- entities/Sistema.py
+++ entities/Sistema.py
@@ -1,9 +1,11 @@
 from entities.Pieza import Pieza
+from entities.Maquina import Maquina
 
 class Sistema:
     def __init__(self):
         self.piezas = []
-
+        self.maquinas = []
+#No registrar 2 veces la misma pieza
     def registrar_pieza(self):
         descripcion = input("Descripción: ")
         costo = float(input("Costo por unidad: "))
@@ -12,6 +14,12 @@ class Sistema:
 
         nueva_pieza = Pieza(descripcion, costo, tamanio_lote, cantidad)
         self.piezas.append(nueva_pieza)
-        print(f"Pieza registrada ({nueva_pieza.codigo}).")
+        print(f"Pieza registrada {nueva_pieza.codigo}.")
+
+    def registrar_maquina(self):
+        descripcion = input("Descripcion: ")
+        nueva_maquina = Maquina(descripcion)
+        self.maquina.append(nueva_maquina)
+        print(f"Maquina registrada {nueva_maquina.codigo}.")
 
     
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
                 if opcion_registrar == "1":
                     sistema.registrar_pieza()
                 elif opcion_registrar == "2":
-                    # registrar_maquina()
+                    sistema.registrar_maquina()
                     pass
                 elif opcion_registrar == "3":
                     # registrar_cliente()
