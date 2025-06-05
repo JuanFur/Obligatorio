f71a563a353613c11c1c870273e7e06dc2ea60c6
Nacho2917 <ignacioreyesfalero@gmail.com>
2025-06-05 09:54:45 -0300

    hoy

--- entities/Maquina.py
+++ entities/Maquina.py
@@ -1,24 +1,3 @@
-"""
-Máquina
-Cada máquina deberá tener un código único numérico, una descripción, una lista de
-requerimientos de piezas con las respectivas cantidades necesarias para construirla y su
-costo de producción.
-Se piden los siguientes datos por terminal:
-- Descripción
-- Agregar requisito de pieza (continuar pidiendo hasta que el usuario seleccione “No”)
-- Sí
-- Pieza (listar piezas para poder seleccionar una ingresando su código)
-- Cantidad
-- No
-Nuevamente, el código de máquina es generado automáticamente y la descripción debe ser
-única. Para seleccionar una pieza, se listan todas las existentes para que el usuario pueda
-seleccionar una según el código de pieza correspondiente. Al agregar un nuevo requisito de
-pieza, deben ocultarse del listado las piezas ya seleccionadas (para evitar duplicados).
-El costo de producción se obtiene directamente como la suma de los costos unitarios de
-cada pieza multiplicados por la cantidad necesaria (se omiten costos asociados al proceso
-de ensamblado).
-"""
-
 
 from entities.Requerimiento import Requerimiento
 
@@ -29,8 +8,8 @@ class Maquina:
         self.requerimientos = []
 
     def agregar_requerimiento(self, pieza, cantidad):
-        requerimiento = Requerimiento
+        requerimiento = Requerimiento(pieza, self,cantidad)
         self.requerimientos.append(requerimiento)
- 
+
     def costo_produccion(self):
-        return sum(requerimiento.pieza.costo*requerimiento.cantidad for requerimiento in self.requerimientos)
+        return sum(requerimiento.pieza.costo*requerimiento.cantidad)
