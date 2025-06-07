a8e82e974f33e213d0bb5a1ffb2949efd80dee35
Gaspar512 <gasparreimundez@icloud.com>
2025-06-06 23:18:48 -0300

    Clientes con cambios

--- entities/Clientes.py
+++ entities/Clientes.py
@@ -8,9 +8,16 @@ def nombre_valido(nombre):
         raise ExceptionClienteYaExiste("El nombre debe contener solo letras")
     return nombre
 def cedula_valida(cedula):
-    if len(cedula) != 8 or not cedula.isdigit():
+    if len(cedula) != 8 and not cedula.isdigit():
         raise ValueError("Debe tener 8 digitos numericos")
     return cedula
+def telefono_valido(telefono):
+    if len(telefono) != 9 and not telefono.isdigit():
+        raise ValueError("Debe ser de 9 digitos numericos")
+    
+def rut_valido (rut):
+    if len(rut) != 12 and not rut.isdigit():
+        raise ValueError ("Debe ser de 12 digitos numericos")
     
     
 def generar_id():
